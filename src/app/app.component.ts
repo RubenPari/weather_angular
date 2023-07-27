9c38778b4ec1069e72a66b59447486f87da6c713
PARRUB <ruben.pari@zucchetti.it>
2023-07-27 14:30:10 +0200

    43:00

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'weather_angular';
+export class AppComponent implements OnInit {
+  cityName: string | undefined;
+  weatherData: any;
+
+  constructor(private weatherService: WeatherService) { }
+
+  ngOnInit() {
+    this.weatherService.getWeatherData('London')
+      .subscribe({
+        next: (response: any) => {
+          this.weatherData = response;
+        }
+      }
+      );
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  weatherBaseUrl: 'https://api.openweathermap.org/data/2.5/weather',
+  xRapidApiKey: 'c2e4a4f3a2mshb0b0b0b0b0b0b0b0b0b0b0jsn1a3e2b0b0b0b0b',
+  xRapidApiHeaderName: 'x-rapidapi-key',
+  xRapidApiHeaderValue: 'c2e4a4f3a2mshb0b0b0b0b0b0b0b0b0b0b0jsn1a3e2b0b0b0b0b'
+};
